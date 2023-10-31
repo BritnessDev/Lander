f1ba75bb29550685475df3ae5914ef6b36a228ae
BritnessDev <britness.gmd@gmail.com>
2023-10-31 11:53:44 -0700

    feat: change the email

--- sendLead.php
+++ sendLead.php
@@ -61,7 +61,7 @@ if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 
 	// Send the data to google sheet
 	$ch = curl_init();
-	curl_setopt( $ch, CURLOPT_URL, 'https://script.google.com/macros/s/AKfycbzeLuy4YtFRd7StrRXcUAuN3BVQzzdj9hZKD-77zGq8rRBUW995UFgQu6Khy2_BdIOcBA/exec' );
+	curl_setopt( $ch, CURLOPT_URL, 'https://script.google.com/macros/s/AKfycby5NqLLIYZNr3s3zrGdPhbrfyiJc0Sm8UZAT0Gb_-zNW175HZnZCvsQwDcMz4EQMGwtHA/exec' );
 	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 	curl_setopt( $ch, CURLOPT_POST, true );
 	curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
